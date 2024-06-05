bae953099f23084f2ccad44404530187d27d2fb1
astri <astri@astri.com>
2024-06-05 20:49:00 +0700

    blog

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,64 +1,49 @@
+import { MdArrowOutward } from "react-icons/md";
 import Project1img from '../assets/project1.jpg';
 import Project2img from '../assets/project2.jpg';
-import Project3img from '../assets/project3.jpg';
 
 export default function Blog() {
   return (
     <>
-        <div className="flex p-10 space-x-10 
-    lg:flex-row ssm:flex-col ssm:space-y-10">
-            <div className="lg:w-1/2 ssm:w-fit">
+        <div className="flex p-10 space-x-10 lg:flex-row ssm:flex-col ssm:space-y-10 items-center w-full ">
+            <div className="lg:w-1/2 w-full flex lg:justify-start justify-center lg:items-center ">
             <h1 className="text-5xl">From my blog</h1>
             </div>
-            <div className="w-1/2 items-center ssm:w-fit">
-                <button className='border border-black text-black px-4 py-3 rounded '>View All</button>
+            <div className="lg:w-1/2 ssm:w-fit flex justify-end">
+                <button className='flex items-center border border-black text-black px-8 py-3 '>View All<MdArrowOutward/></button>
             </div>
         </div>
 
         <div>
-            <div className='grid grid-cols-3 gap-4 p-5'>
-                <div className='max-w-md mx-auto bg-neutral-100 rounded-1 
-                shadow-md oberflow-hidden md-max-w-2xl hover:animate-pulse'>
-                <div className='md:flex'>
-                    <div className='p-8'>
-                    <div>
-                    <img src={Project2img} alt='' />
-                    </div>
-                    <a href='#' className='text-3xl text-center'>
-                     User Interface Design</a>
-                    </div>
-                </div>
-                </div>
-                <div className='max-w-md mx-auto bg-neutral-100 rounded-1 
-                shadow-md oberflow-hidden md-max-w-2xl hover:animate-pulse'>
-                <div className='md:flex'>
-                    <div className='p-8'>
-                    <div>
-                    <img src={Project1img} alt='' />
-                    </div>
-                    <a href='#' className='text-3xl text-center '>
-                     User Interface Design</a>
-                    </div>
-                </div>
-                </div>
-                <div className='max-w-md mx-auto bg-neutral-100 rounded-1 
-                shadow-md oberflow-hidden md-max-w-2xl hover:animate-pulse'>
-                <div className='md:flex'>
-                    <div className='p-8'>
-                    <div>
-                    <img src={Project3img} alt='' />
-                    </div>
-                    <a href='#' className='text-3xl text-center '>
-                     User Interface Design</a>
-                    </div>
-                </div>
-                </div>
+            <div className='grid grid-cols-2 gap-4 p-5 mb-10'>
+            <div className="bg-neutral-100 flex items-center space-x-10 
+    lg:flex-row ssm:flex-col ssm:space-y-10 mb-12">
+            <div className="w-1/2 items-center ssm:w-fit">
+                <img src={Project1img} alt="" width={300} height={200}
+                    className=""
+                />
+            </div>
+            <div className="lg:w-1/2 ssm:w-fit">
+            <h1 className="text-3xl">IKEA – Designing dashboard for supply chain management</h1>
+            <hr/>
+            </div>
+        </div>
+        <div className="bg-neutral-100 flex items-center space-x-10 
+    lg:flex-row ssm:flex-col ssm:space-y-10 mb-12">
+            <div className="w-1/2 items-center ssm:w-fit">
+                <img src={Project2img} alt="" width={300} height={200}
+                    className=""
+                />
+            </div>
+            <div className="lg:w-1/2 ssm:w-fit">
+            <h1 className="text-3xl">IKEA – Designing dashboard for supply chain management</h1>
+            <hr/>
+            </div>
+        </div>
             </div>
             <hr/>
         </div>
     </>
   )
-
-
 }
 
